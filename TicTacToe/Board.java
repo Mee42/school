a1f3fc6c9fd099fd9f547b513fe335fe52e245c4
Carson Graham <carson42g@gmail.com>
2019-03-05 09:47:28 -0500

    Update Board.java

--- TicTacToe/Board.java
+++ TicTacToe/Board.java
@@ -1,6 +1,17 @@
 class Board{
-  private int[] board;
-  private int turn;
+  private int[] board;//store in 1d array. 
+  /*
+ 0 | 1 | 2 
+ ----------
+ 3 | 4 | 5
+ ----------
+ 6 | 7 | 8
+  
+  0 = empty
+  1 = player 1
+  2 = player 2
+  */
+  private int turn;//keep the turn in this class
 
   Board(){
     board = new int[9];
@@ -8,16 +19,16 @@ class Board{
   }
 
   public boolean place(int place){
-    if(place < 0 || place > 8)return false;
-    if(board[place] != 0)return false;
-    board[place] = turn;
-    turn = (turn == 1) ? 2 : 1;
+    if(place < 0 || place > 8)return false;//don't move if out of bounds
+    if(board[place] != 0)return false;//don't move if spot is already filled
+    board[place] = turn;//move
+    turn = (turn == 1) ? 2 : 1;//ONLY CHANGE IF THE PLAYER PLAYED
     return true;
   }
 
 
   public int winner(){
-    int[] b = board;
+    int[] b = board;//for shorter notation
     //rows
     if(b[0] == b[1] && b[1] == b[2])return b[0];
     if(b[3] == b[4] && b[4] == b[5])return b[3];
@@ -35,7 +46,7 @@ class Board{
   public boolean hasAnyoneWon(){ return winner() != 0; }
   
   public boolean isFull(){
-    int[] b = board;
+    int[] b = board;//for shorter notation
     return b[0] != 0 &&
         b[1] != 0 &&
         b[2] != 0 &&
@@ -50,12 +61,12 @@ class Board{
 
   public String getTurn() { return s(turn); }
 
-
+  /** Turns the integer passed into a string to make the output pretty. See public String getTurn() as well */
   public String s(int i){return (i == 1) ? "X" : (i == 0) ? " " : "O"; } 
   
   @Override
   public String toString(){
-    int[] b = board;
+    int[] b = board;//for shorter notation
     return "" +
         s(b[0]) + " | " + s(b[1]) + " | " + s(b[2]) + "\n" +
         "----------\n" +
