c88fa307cd40bb707064f2597fb2d8cf0a5bd061
Carson Graham <carson42g@gmail.com>
2019-03-07 17:12:16 -0500

    Update Pair.java

--- Minesweaper/Pair.java
+++ Minesweaper/Pair.java
@@ -1,3 +1,9 @@
+//Imutable. 
+
+// See:
+// - https://stackoverflow.com/questions/6044923/generic-pair-class
+// - https://kotlinlang.org/api/latest/jvm/stdlib/kotlin/-pair/index.html
+
 class Pair<A,B>{
   public final A a;
   public final B b;
